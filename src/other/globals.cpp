4eaa1ca8c12c338ef57c1daad880963e042f7b92
Simon <eiderdaus@gmail.com>
2011-07-14 03:09:40 +0200

    two drowner animations drawn

--- src/other/globals.cpp
+++ src/other/globals.cpp
@@ -17,8 +17,8 @@ Globals* gloB(0);
 
 Globals::Globals()
 :
-    version              (CONCAT(2011, 07, 13, 00)),
-    version_min          (CONCAT(2011, 07, 13, 00)),
+    version              (CONCAT(2011, 07, 14, 00)),
+    version_min          (CONCAT(2011, 07, 14, 00)),
 
     updates_per_second   (15),
     skill_max            (12),
