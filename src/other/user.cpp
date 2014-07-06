0cf103fca3d0a0d95eafcc6718267c251609d84a
Simon <eiderdaus@gmail.com>
2014-07-06 07:48:41 +0200

    mouse accel on Windows default 0

--- src/other/user.cpp
+++ src/other/user.cpp
@@ -108,7 +108,7 @@ User::User()
     replay_cancel          (true),
     replay_cancel_at       (30),
     mouse_speed            (20),
-    mouse_acceleration     (1),
+    mouse_acceleration     (0),
     scroll_speed_edge      (5),
     scroll_speed_click     (6),
     scroll_torus_x         (2),
