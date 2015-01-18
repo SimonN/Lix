881d812e02be5fab89f4c3de6add39c87a3e84e6
Simon <eiderdaus@gmail.com>
2015-01-18 22:47:47 +0100

    bugfix: floater landed on vertical wall, was assignable

--- src/lix/floater.cpp
+++ src/lix/floater.cpp
@@ -31,32 +31,58 @@ void update_floater(Lixxie& l, const UpdateArgs& ua)
         default:                    break;
     }
 
-    // Horizontale Bewegung auf Dauer zum Stillstand kommen lassen,
-    // die for-Schleife hier kontrolliert auf sehr schnellen Stillstand
-    for (int i = 2; i < l.get_special_x(); i += 2)
-     if (l.is_solid(i, 0)) {
-        l.set_special_x(i-2);
-        break;
+    if (l.get_special_x() > 0 && l.get_frame() > 0) {
+        l.set_special_x(Help::even(l.get_special_x() - 2));
     }
-    if (l.get_special_x() > 0 && l.get_frame() > 0)
-     l.set_special_x(Help::even(l.get_special_x() - 2));
+    // special x and y have been set to new values for the motion further
+    // down in this function. They have ignored terrain in the way so far,
+    // that must be corrected during the motion.
 
-    // Auf Boden prüfen,
-    // dabei wird diagonal nach unten geschritten und davon ausgegangen,
-    // dass die Geschwindigkeit in y-Richtung > Geschw. in x-Richtung ist
-    for (int i = 0; i < l.get_special_y(); ++i) {
-        int x = (l.get_special_x() * i) / l.get_special_y();
-        if (l.is_solid(x, i+2)) {
+    // The following code is a little like jumper_and_tumbler_collision.
+    // I don't dare to use that code for floaters too. It's already very
+    // complicated and many of its corner cases don't apply to floaters.
+    // spx tracks the special_x to be used this update, which may not always
+    // be the same as l.get_special_x() if a wall is hit.
+          int spx    = l.get_special_x();
+    const int spy    = l.get_special_y();
+    const int sp_max = std::max(spx, spy);
+
+    bool floor_hit = false;
+    bool wall_hit  = false;
+
+    for (int i = 0; i < sp_max; ++i) {
+        // move either 1 pixel down, 1 pixel ahead, or 1 pixel both. This
+        // takes max(spx, spy) chess king moves to get to the target square.
+        // Therefore i runs to max(spx, spy).
+        // We don't move during this loop, except upon hitting floor. The
+        // normal movement occurs after this loop. This is unlike jumper_and_
+        // _tumbler_collision.
+        const int x = (wall_hit ? spx : spx * i / sp_max);
+        const int y =                   spy * i / sp_max;
+        // check for floor
+        if (l.is_solid(x, y + 2)) {
             l.move_ahead(x);
-            l.move_down(i);
+            l.move_down(y);
             l.become(LixEn::LANDER);
+            floor_hit = true;
             break;
         }
+        // check for wall
+        else if (l.is_solid(x + 2, y)) {
+            wall_hit = true;
+            l.set_special_x(0);
+            spx = x;
+            // now the current value of x is frozen for this update, see
+            // definition of const int x = (wall hit ? spx : ...), and all
+            // future updates will use l.get_special_x() == spx == x == 0.
+        }
     }
-
+    if (! floor_hit) {
+        l.move_ahead(spx);
+        l.move_down (spy);
+    }
+    // During motion, we could have become a lander. Otherwise:
     if (l.get_ac() == LixEn::FLOATER) {
-        l.move_ahead(l.get_special_x());
-        l.move_down(l.get_special_y());
         if (l.is_last_frame()) l.set_frame(9);
         else l.next_frame();
     }
