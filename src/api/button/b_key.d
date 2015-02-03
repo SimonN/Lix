abdbc1670d6603d685543c3e12ef720fa371bd4b
Simon <eiderdaus@gmail.com>
2015-02-03 11:20:13 +0100

    removed old .d files from source tree

--- src/api/button/b_key.d
+++
@@ -1,9 +0,0 @@
-api/button/b_key.o: api/button/b_key.cpp api/button/b_key.h \
- api/button/b_text.h api/button/../../other/myalleg.h api/button/button.h \
- api/button/../element.h api/button/../../other/hardware.h \
- api/button/../../other/../graphic/glob_gfx.h \
- api/button/../../other/../graphic/cutbit.h \
- api/button/../../other/../graphic/torbit.h \
- api/button/../../other/globals.h api/button/../manager.h \
- api/button/../../other/help.h api/button/../../other/../network/net_t.h \
- api/button/../../other/language.h
