45265121a4c0303fec9ebcb5a1ba7903a0938396
SimonN <eiderdaus@gmail.com>
2015-06-04 02:48:01 +0200

    version to 2015-06-04

--- src/other/globals.cpp
+++ src/other/globals.cpp
@@ -19,7 +19,7 @@ Globals* gloB(0);
 
 Globals::Globals()
 :
-    version              (CONCAT(2015, 06, 02, 00)),
+    version              (CONCAT(2015, 06, 04, 00)),
     version_min          (CONCAT(2015, 01, 25, 00)),
     version_stable       (true),
 
