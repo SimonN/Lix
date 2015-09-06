230051b38906f737bf075dacd74eef0965203fdd
geoo89 <geoo89@gmail.com>
2015-09-07 00:57:22 +0200

    Diggers will now fall instantly when terrain is removed under them.

--- src/lix/digger.cpp
+++ src/lix/digger.cpp
@@ -18,46 +18,68 @@ void become_digger(Lixxie& l)
 
 
 
+bool digger_hit_steel(Lixxie& l, const UpdateArgs& ua) {
+    // Stop digging if there is steel to the left as well as to the right
+    // of the digger's center, or in the 3 double pixels of the center.
+    // The following two rectangles overlap, that implements the center.
+    int steel_left   = l.count_steel(-8, 2, 3, 2);
+    int steel_right  = l.count_steel(-2, 2, 9, 2);
+
+    if (steel_left > 0 && steel_right > 0) {
+        l.become(LixEn::WALKER);
+        l.play_sound(ua, Sound::STEEL);
+        l.get_ef()->add_dig_hammer(ua.st.update, l.get_tribe(), ua.id,
+            l.get_ex(), l.get_ey() - 10);
+        return true; // from update_digger() entirely
+        // if steel cancelling happens on the very first swing, the upswing
+        // (next if) doesn't happen
+    }
+
+    return false;
+}
+
+
+bool digger_should_fall(Lixxie& l) {
+    if (! l.is_solid(-2, 2)
+     && ! l.is_solid( 0, 2)
+     && ! l.is_solid( 2, 2)
+    ) {
+        l.become(LixEn::FALLER);
+        return true;
+    }
+
+    return false;
+}
+
+
 void update_digger(Lixxie& l, const UpdateArgs& ua)
 {
     // there's only one frame with all the digging action
     if (l.get_frame() != 11) {
-        l.next_frame();
-        return;
-    }
-
-    // we're in frame 11, where the action happens
-    for (int y = 0; y < 4; ++y) {
-        // Stop digging if there is steel to the left as well as to the right
-        // of the digger's center, or in the 3 double pixels of the center.
-        // The following two rectangles overlap, that implements the center.
-        int steel_left   = l.count_steel(-8, 2, 3, 2);
-        int steel_right  = l.count_steel(-2, 2, 9, 2);
-
-        if (steel_left > 0 && steel_right > 0) {
-            l.become(LixEn::WALKER);
-            l.play_sound(ua, Sound::STEEL);
-            l.get_ef()->add_dig_hammer(ua.st.update, l.get_tribe(), ua.id,
-                l.get_ex(), l.get_ey() - 10);
-            return; // from update_digger() entirely
-            // if steel cancelling happens on the very first swing, the upswing
-            // (next if) doesn't happen
-        }
-        l.remove_rectangle(-8, 2, 9, 2);
-        if (l.get_special_x() == 1) {
-            // Remove extra earth upon first swing: four hi-res rows, like L1.
-            l.remove_rectangle(-8, -2, 9, 1);
-            l.set_special_x(0);
-        }
-        l.move_down(1);
-
-        if (! l.is_solid(-2, 2)
-         && ! l.is_solid( 0, 2)
-         && ! l.is_solid( 2, 2)
-        ) {
-            l.become(LixEn::FALLER);
+        if (digger_should_fall(l)) {
             return;
         }
     }
+    else {
+        // we're in frame 11, where the action happens
+        for (int y = 0; y < 4; ++y) {
+            if (digger_hit_steel(l, ua)) {
+                return;
+            }
+
+            l.remove_rectangle(-8, 2, 9, 2);
+            if (l.get_special_x() == 1) {
+                // Remove extra earth upon first swing: four hi-res rows, like L1.
+                l.remove_rectangle(-8, -2, 9, 1);
+                l.set_special_x(0);
+            }
+            l.move_down(1);
+            
+            if (digger_should_fall(l)) {
+                return;
+            }
+        }
+    }
+
     l.next_frame();
 }
